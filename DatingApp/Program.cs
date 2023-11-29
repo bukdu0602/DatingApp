f9505f412b1eafec62173a50c49619aa89a2d434
Ryan Lim <ryan.hyun@gmail.com>
2023-11-28 20:52:22 -0800

    end of section 7

--- DatingApp/Program.cs
+++ DatingApp/Program.cs
@@ -1,11 +1,5 @@
-using DatingApp.Data;
 using DatingApp.Extensions;
-using DatingApp.Interfaces;
-using DatingApp.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
+using DatingApp.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +12,8 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
 .WithOrigins("http://localhost:4200"));
 
